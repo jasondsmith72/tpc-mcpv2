7e64dac68f06e5766a7e05dd7212516effd29aeb
jasondsmith72 <jason.smith@mtusa.com>
2025-03-30 06:46:09 -0700

    Add tools index file

---
+++ src/tools/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Export all tools from their respective modules
+ */
+
+// Screen tools
+export * from "./screen.js";
+
+// Mouse tools
+export * from "./mouse.js";
+
+// Keyboard tools
+export * from "./keyboard.js";
+
+// Clipboard tools
+export * from "./clipboard.js";
